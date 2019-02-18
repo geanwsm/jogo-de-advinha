b38498974cff02801f69cf2b2a0cb424cc395997
Gean Willian Santos Matos <972060@PPC01INFLL00318.pucminas.net>
2019-02-18 20:07:49 -0300

    #1: três tentativas implementadas

--- advinha.rb
+++ advinha.rb
@@ -1,11 +1,65 @@
 segredo = 151
+tentativas = 1
 
 puts("Digite seu nome: ")
 
 nome = gets
 
-puts("digite o valor, "+ nome +": ")
+#tent 1
+
+puts("\ndigite o valor, "+ nome +": ")
 
 chute = gets
 
-puts(segredo == chute.to_i)
+puts("tentativa: "+ tentativas.to_s)
+
+if segredo == chute.to_i
+  puts("Você acertou!")
+elsif segredo > chute.to_i
+  puts("o valor inserido é menor.")
+elsif segredo < chute.to_i
+  puts("o valor inserido é maior.")
+else
+  puts("você errou!")
+end
+
+tentativas += 1
+
+#tent 2
+
+puts("\ndigite o valor, "+ nome +": ")
+
+chute = gets
+
+puts("tentativa: "+ tentativas.to_s)
+
+if segredo == chute.to_i
+  puts("Você acertou!")
+elsif segredo > chute.to_i
+  puts("o valor inserido é menor.")
+elsif segredo < chute.to_i
+  puts("o valor inserido é maior.")
+else
+  puts("você errou!")
+end
+
+tentativas += 1
+
+#tent 3
+
+puts("\ndigite o valor, "+ nome +": ")
+
+chute = gets
+
+puts("tentativa: "+ tentativas.to_s)
+
+if segredo == chute.to_i
+  puts("Você acertou!")
+elsif segredo > chute.to_i
+  puts("o valor inserido é menor.")
+elsif segredo < chute.to_i
+  puts("o valor inserido é maior.")
+else
+  puts("você errou!")
+end
+
