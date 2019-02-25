7015df71faa88d957f2b0762212f319eac68cf59
Gean Willian Santos Matos <972060@PPC01INFLL00318.pucminas.net>
2019-02-25 19:56:03 -0300

    #2: tentativas com laço de repetição.

--- advinha.rb
+++ advinha.rb
@@ -7,59 +7,23 @@ nome = gets
 
 #tent 1
 
-puts("\ndigite o valor, "+ nome +": ")
+while tentativas < 4
 
-chute = gets
+  puts("\ndigite o valor, "+ nome +": ")
 
-puts("tentativa: "+ tentativas.to_s)
+  chute = gets
 
-if segredo == chute.to_i
-  puts("Você acertou!")
-elsif segredo > chute.to_i
-  puts("o valor inserido é menor.")
-elsif segredo < chute.to_i
-  puts("o valor inserido é maior.")
-else
-  puts("você errou!")
+  puts("tentativa: "+ tentativas.to_s)
+
+  if segredo == chute.to_i
+    puts("Você acertou!")
+  elsif segredo > chute.to_i
+    puts("o valor inserido é menor.")
+  elsif segredo < chute.to_i
+    puts("o valor inserido é maior.")
+  else
+    puts("você errou!")
+  end
+
+  tentativas += 1
 end
-
-tentativas += 1
-
-#tent 2
-
-puts("\ndigite o valor, "+ nome +": ")
-
-chute = gets
-
-puts("tentativa: "+ tentativas.to_s)
-
-if segredo == chute.to_i
-  puts("Você acertou!")
-elsif segredo > chute.to_i
-  puts("o valor inserido é menor.")
-elsif segredo < chute.to_i
-  puts("o valor inserido é maior.")
-else
-  puts("você errou!")
-end
-
-tentativas += 1
-
-#tent 3
-
-puts("\ndigite o valor, "+ nome +": ")
-
-chute = gets
-
-puts("tentativa: "+ tentativas.to_s)
-
-if segredo == chute.to_i
-  puts("Você acertou!")
-elsif segredo > chute.to_i
-  puts("o valor inserido é menor.")
-elsif segredo < chute.to_i
-  puts("o valor inserido é maior.")
-else
-  puts("você errou!")
-end
-
