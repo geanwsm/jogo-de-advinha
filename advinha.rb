ca44c7c5a3d084bd99a7ea3096f1f1f56b0dcf7b
Gean Willian Santos Matos <972060@PPC01INFLL00318.pucminas.net>
2019-02-25 20:05:48 -0300

    #3: modularizar o código.

--- advinha.rb
+++ advinha.rb
@@ -1,29 +1,35 @@
+def advinhar(nome, segredo)
+
+  tentativas = 1
+  while tentativas < 4
+
+    puts("\ndigite o valor, "+ nome.to_s)
+
+    chute = gets
+
+    puts("tentativa: "+ tentativas.to_s)
+
+    if segredo == chute.to_i
+      puts("Você acertou!")
+    elsif segredo > chute.to_i
+      puts("o valor inserido é menor.")
+    elsif segredo < chute.to_i
+      puts("o valor inserido é maior.")
+    else
+      puts("você errou!")
+    end
+
+    tentativas += 1
+  end
+
+end
+
 segredo = 151
-tentativas = 1
 
 puts("Digite seu nome: ")
 
 nome = gets
 
-#tent 1
+advinhar(nome, segredo)
 
-while tentativas < 4
 
-  puts("\ndigite o valor, "+ nome +": ")
-
-  chute = gets
-
-  puts("tentativa: "+ tentativas.to_s)
-
-  if segredo == chute.to_i
-    puts("Você acertou!")
-  elsif segredo > chute.to_i
-    puts("o valor inserido é menor.")
-  elsif segredo < chute.to_i
-    puts("o valor inserido é maior.")
-  else
-    puts("você errou!")
-  end
-
-  tentativas += 1
-end
