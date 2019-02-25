01df3c9246243df2c072a741e34f6fcde9c7276c
Gean Willian Santos Matos <972060@PPC01INFLL00318.pucminas.net>
2019-02-25 20:35:10 -0300

    #4: perguntar ao usuário se ele deseja jogar novamente.

--- advinha.rb
+++ advinha.rb
@@ -1,16 +1,17 @@
 def advinhar(nome, segredo)
 
   tentativas = 1
-  while tentativas < 4
+  continuar = 0
 
+  while tentativas < 4
+    puts("tentativa: "+ tentativas.to_s)
     puts("\ndigite o valor, "+ nome.to_s)
 
     chute = gets
 
-    puts("tentativa: "+ tentativas.to_s)
-
     if segredo == chute.to_i
       puts("Você acertou!")
+      break
     elsif segredo > chute.to_i
       puts("o valor inserido é menor.")
     elsif segredo < chute.to_i
@@ -18,10 +19,25 @@ def advinhar(nome, segredo)
     else
       puts("você errou!")
     end
+      
+      tentativas += 1
 
-    tentativas += 1
+    if tentativas.to_i == 4
+      puts("Deseja continuar?\n  1.sim\n  2.não")
+      continuar = gets
+      
+      while continuar.to_i <0 || continuar.to_i > 2
+        puts("\nopção incorreta!\n")
+        puts("Deseja continuar?\n  1.sim\n  2.não")
+        continuar = gets
+      end
+       
+      if continuar.to_i == 1
+          tentativas = 1
+      elsif continuar.to_i == 2
+      end
+    end
   end
-
 end
 
 segredo = 151
